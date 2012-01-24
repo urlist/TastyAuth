e08dc584defa3912972ca9e6a1ab1a6770425d89
vrde <agranzot@gmail.com>
2012-01-24 17:37:39 +0100

    Facebook users may not have a username, sad pandas

--- tastyauth/auth.py
+++ tastyauth/auth.py
@@ -102,7 +102,7 @@ class Facebook(object):
             container['parsed'] = {
                 'uid': user['id'],
                 'email': user.get('email'),
-                'username': user['username'],
+                'username': user.get('username'),
                 'screen_name': user['name'],
                 'first_name': user['first_name'],
                 'last_name': user['last_name'],
